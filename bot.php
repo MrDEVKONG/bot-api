c470fd72fba6ba83e3d8d34e850d9186e158b471
MrDEVKONG <davidlundberg@DAT-1590.local>
2017-01-25 13:45:08 +0200

    Skapade en chat-bot

---
+++ bot.php
@@ -0,0 +1,6 @@
+<?php 
+
+header("Content-type: text/plain; charset=utf=8;");
+echo "Hej!";
+
+?>p
---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	var jqxhr = $.ajax( "example.php" )
+	.done(function() {
+		alert( "success" );
+	})
+	.fail(function() {
+		alert( "error" );
+	})
+	.always(function() {
+		alert( "complete" );
+	});
+});
