97445ed5f10bf5a62f9308cd74a61b3a78b96501
MrDEVKONG <davidlundberg@DAT-1590.local>
2017-01-26 14:37:58 +0200

    En dat chatbot

--- bot.php
+++ bot.php
@@ -1,6 +1,13 @@
 <?php 
 
 header("Content-type: text/plain; charset=utf=8;");
-echo "Hej!";
 
-?>p
+if ($_GET["meme"] == null) {
+	echo "Hejsan!";
+} else {
+	$memes = ["Thanks Obama!","9+10=21","5/7 perfekt score!","Datanom is memes", "Kongo", "Nikelback", "Are U A Wizard?", "POO", "Cat videos are 'CENCURED'!", "<img src=\"img/download.jpeg\">", "<img src=\"img/download1.jpeg\">", "<img src=\"img/images.jpeg\">", "<img src=\"img/images1.jpeg\">"];
+	echo $memes[random_int(0, count($memes) - 1)];
+}
+
+?>
+
--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
 	var jqxhr = $.ajax( "bot.php" )
-	.done(function() {
+	.done(function(meddelande) {
 		console.log( "success" );
+		console.log( meddelande );
+		$("#chat_log").prepend( meddelande + "<br>" );
 	})
 	.fail(function() {
 		console.log( "error" );
 	})
 	.always(function() {
 		console.log( "complete" );
+	});
+
+	jQuery("#meme_form").submit(function(event) {
+		var user_meme = event.target[0].value;
+		jQuery.ajax("bot.php", {data: {meme: user_meme}}).done(function(meddelande) {
+			console.log(meddelande);
+			$("#chat_log").prepend( meddelande + "<br>" );
+		});
+		event.target[0].value = "";
+		event.preventDefault();
 	});
 });
